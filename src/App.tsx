5631b86b005c1359b28e633cb133a8d3aab984ee
raulfq <33738907+raulfq@users.noreply.github.com>
2025-12-11 02:44:24 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function LandingSistemaAvicola() {
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto px-6 py-5 flex items-center justify-between">
           <div className="flex items-center gap-3">
-           <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-orange-400 to-orange-600 flex items-center justify-center">
+           <div className="w-10 h-10 rounded-lg  from-orange-400 to-orange-600 flex items-center justify-center">
   <img src="/logo.png" alt="Logo" className="w-8 h-8 object-contain" />
 </div>
 
